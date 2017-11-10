b114a1ab551a24020b24466dd0cb39605b6d9703
doughfeet <kennethblix@gmail.com>
2017-11-10 13:15:18 +0100

    GetApplicationData, dataGrindView showing data from table..

--- ApplicationStorer/ApplicationStorer/Form1.cs
+++ ApplicationStorer/ApplicationStorer/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,31 @@ namespace ApplicationStorer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = GetApplicationData();
+        }
+
+        private DataTable GetApplicationData()
+        {
+            DataTable dtApplication = new DataTable();
+
+            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\kenne\Source\Repos\ApplicationStorer\ApplicationStorer\ApplicationStorer\Data\ApplicationData.mdf; Integrated Security = True; Connect Timeout = 30";
+            string query = "SELECT * FROM ApplicationTable";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                dtApplication.Load(reader);
+            }
+
+            return dtApplication;
+
         }
     }
 }
